6217a477aae9ae8101dc12056bd8a75d8bb12882
reginauwu <wregina0418@gmail.com>
2023-02-14 23:17:53 -0500

    enemy shooting and player invisibility

--- CharacteristicsOfGames/Assets/Code/playerController.cs
+++ CharacteristicsOfGames/Assets/Code/playerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public static playerController p;
 
     Rigidbody2D _rigidbody;
     Animator _animator;
     SpriteRenderer sprite;
+    Color origColor;
     int speed = 8;
     int jumpForce = 800;
 
@@ -16,11 +18,18 @@ public class playerController : MonoBehaviour
     float groundCheckDist = 1f; // bigger val = for wall stick/climb
     bool grounded = false;
 
+    public bool invis = false;
+    float stamina = 10;
+
+    //private Color origColor;
+
     void Start()
     {
+        p = this;
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        origColor = GetComponent<Renderer>().material.color;
     }
 
     private void FixedUpdate() {
@@ -48,6 +57,31 @@ public class playerController : MonoBehaviour
         // check if grounded and jumping
         if (grounded && Input.GetButtonDown("Jump")) {
             _rigidbody.AddForce(new Vector2(0, jumpForce));
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && !invis) {
+            invis = true;
+        } 
+
+        if (Input.GetKeyDown(KeyCode.G) && invis) {
+            invis = false;
+        } 
+
+        if (invis) {
+            gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
+
+            stamina -= Time.deltaTime * 2;
+            print(stamina);
+
+            if (stamina < 0) {
+                invis = false;
+            }
+        } else {
+            gameObject.GetComponent<Renderer>().material.color = origColor;
+            if (stamina < 10) {
+                stamina += Time.deltaTime;
+                print(stamina);   
+            }
         }
     }
 }
---
+++ CharacteristicsOfGames/Assets/Code/shooting.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D _rigidbody;
+    private float timer;
+
+    private float flashTimer;
+    
+    public float force = 10;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 dir = player.transform.position - transform.position;
+        _rigidbody.velocity = new Vector2(dir.x, dir.y).normalized * force;
+
+        float rot = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 5) {
+            Destroy(gameObject);
+        }
+    }
+
+    // if bullet collides with player, destroy
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player") || (other.gameObject.CompareTag("Platforms"))) {
+            Destroy(gameObject);
+        }
+    }
+}
